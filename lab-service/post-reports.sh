8246f426cbceb39b98633a8befe26ebbb6b9f613
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-07-30 23:35:34 -0300

    first commit

---
+++ lab-service/post-reports.sh
@@ -0,0 +1,12 @@
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d "{\"id\": 129999999999999}" \
+  $LAB_REPORT_SERVICE_URL &
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d "{\"id\": 349999999999999}" \
+  $LAB_REPORT_SERVICE_URL &
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d "{\"id\": 5699999999999999}" \
+  $LAB_REPORT_SERVICE_URL &
